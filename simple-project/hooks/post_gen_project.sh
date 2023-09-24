1ddc44cf877c2ca88b8e5c33c3db85778e081034
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-09-24 22:56:11 +0530

    chore: refactor the script to make a local `git` repo

--- simple-project/hooks/post_gen_project.sh
+++ simple-project/hooks/post_gen_project.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+# Initialise the generated project as a local Git repository
+git init
+
 # Generate the project files/folders and install the Pre-Commit hooks
 pre-commit install --install-hooks
