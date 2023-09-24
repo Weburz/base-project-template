34369c904152821bf75e64334710fc6875e8bec2
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-09-24 22:48:15 +0530

    chore: refactor the hook to be print an error message

--- simple-project/hooks/pre_gen_project.sh
+++ simple-project/hooks/pre_gen_project.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 
 # Check if Pre-Commit is installed else stop project generation
 if ! command -v pre-commit --version &>/dev/null; then
+  echo "Please install Pre-Commit before proceeding further!"
   exit 1
 fi
